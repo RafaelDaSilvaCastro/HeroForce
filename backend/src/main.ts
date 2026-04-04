24e68c24ee2137438f6074c792b38b2c83fc0051
Rafael Castro <rafaeldasilvacastroti@gmail.com>
2026-04-04 20:46:59 -0300

    fix: corrigindo erro de cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,8 +12,11 @@ async function bootstrap() {
     }));
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://tranquil-encouragement-production-288f.up.railway.app'],
+    origin: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+
   });
 
   const config = new DocumentBuilder()
