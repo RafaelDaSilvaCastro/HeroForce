2af6aaf63475a382203d037dd9733bba519d2ae4
Rafael Castro <rafaeldasilvacastroti@gmail.com>
2026-04-04 19:31:51 -0300

    feat: implementando ambiente com docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,13 +9,14 @@ import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      database: 'hero_force',
-      username: 'postgres',
-      password: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'postgres',
+      database: process.env.DB_NAME || 'hero_force',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
