e89f9f565953a6e9768527d20f3b4e2330ac6257
Rafael Castro <rafaeldasilvacastroti@gmail.com>
2026-04-04 20:38:42 -0300

    chore: Adicionando url do deplay

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     }));
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://tranquil-encouragement-production-288f.up.railway.app'],
     credentials: true,
   });
 
